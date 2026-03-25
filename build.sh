1dbd50c28030d70b93c3665f27b733402d58f6d2
wamikan <wamikan@icloud.com>
2026-03-25 15:47:23 +0900

    main

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ export PATH="$PATH:`pwd`/flutter/bin"
 
 echo "Flutterのビルドを開始します..."
 # 先ほど設定したAPIキーを含めてWebビルドを実行
-flutter build web --dart-define=GEMINI_API_KEY=AIzaSyCtssUsgKVrrDAcrByzos6wMfD8B0x0Mt8
+flutter build web --dart-define=GEMINI_API_KEY=AIzaSyAx1WY-SXU9wuiovlmmYUPjYRIEimhbqXI
